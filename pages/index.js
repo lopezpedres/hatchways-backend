962b2bcaa0bf82a264fcf10ae1996ba6b2abb101
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-02-12 21:09:59 -0800

    Update Home page

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,7 @@ export default function Home() {
 
   return (
     <div className={ styles.container }>
-      Welcome to my Back End project!
-
-      How it works:
-
-      As required in the intruccion guide, the Api takes 3 arguments:
-      tags
-      sortBy
-      direction
+      Hi there!
     </div>
   )
 }
